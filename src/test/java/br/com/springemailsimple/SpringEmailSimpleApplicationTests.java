3cbc1eb6f531f9fc234d9e2dd0fe04c3ed23e736
Daniel <daniel16henrrique@gmail.com>
2022-07-25 22:57:35 -0300

    Projeto Criado

---
+++ src/test/java/br/com/springemailsimple/SpringEmailSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springemailsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
